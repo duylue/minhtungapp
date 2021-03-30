ec1ac9c869d1c0de87978faab359717e612f5024
duylue <binhminhbenem2801@gmail.com>
2021-03-30 16:47:10 +0700

    srwerwe

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {AnimalComponent} from './animal/animal.component';
+import {ChickenComponent} from './chicken/chicken.component';
+import {NhimComponent} from './nhim/nhim.component';
+import {TigerComponent} from './tiger/tiger.component';
+import {PigComponent} from './pig/pig.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'animal', component: AnimalComponent},
+  {path: 'chicken', component: ChickenComponent},
+  {path: 'nhim', component: NhimComponent},
+  {path: 'tiger', component: TigerComponent},
+  {path: 'pig', component: PigComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AnimalComponent } from './animal/animal.component';
+import { ChickenComponent } from './chicken/chicken.component';
+import { PigComponent } from './pig/pig.component';
+import { TigerComponent } from './tiger/tiger.component';
+import { NhimComponent } from './nhim/nhim.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AnimalComponent,
+    ChickenComponent,
+    PigComponent,
+    TigerComponent,
+    NhimComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
